8957adb1b1ed2e3e03d08ac890d42aa0e099b89b
Saadispark <msaadlashari547@gmail.com>
2025-05-18 07:14:46 +0500

    Add files via upload
    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+const text = ["Tech Visionary", "Student", "Trader", "CSR"];
+let count = 0, index = 0, current = '', letter = '';
+
+(function type() {
+  if (count === text.length) count = 0;
+  current = text[count];
+  letter = current.slice(0, ++index);
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === current.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1000);
+  } else {
+    setTimeout(type, 100);
+  }
+})();
